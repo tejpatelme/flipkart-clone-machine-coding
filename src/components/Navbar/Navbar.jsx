bae9d4240e823976098a0731ff5eeafdc6e30c8f
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-09-05 14:15:51 +0530

    create : navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="w-full z-10 bg-blue-600 h-10 sticky top-0 px-4 flex items-center">
+      <span className="text-white font-bold font-lg">Flipkart Clone</span>
+    </div>
+  );
+}
