c908ba54bdae30cffac83aeec436e7f5c70bfb30
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-09-05 14:15:02 +0530

    create : product card component

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ProductCard({ product }) {
+  return (
+    <div className="w-52 h-auto">
+      <div className="pt-64 relative overflow-hidden">
+        <img
+          src={product.imageURL}
+          alt="product"
+          className="block absolute inset-0 object-cover w-full h-auto"
+        />
+      </div>
+      <div className="px-2">
+        <span className="text-sm font-semibold text-gray-500">
+          {product.brand}
+        </span>
+        <span className="block text-sm">{product.name}</span>
+        <span className="block font-bold text-gray-600 mb-3">
+          ₹{product.price}
+        </span>
+        <span className="block text-sm font-semibold text-gray-500">
+          Size <span className="font-base text-gray-800">{product.size}</span>
+        </span>
+      </div>
+    </div>
+  );
+}
